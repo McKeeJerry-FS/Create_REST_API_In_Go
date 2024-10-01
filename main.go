f0eecbceb052979823c765bb0fdb06232eeaec71
Jerry McKee Jr <jmckeejr@safehomesecurityinc.com>
2024-10-01 11:53:33 -0400

    Replaced net/http with Gorilla/Mux Routers and created the module

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 // Creating an Article struct
@@ -25,13 +26,20 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest(){
 	// homepage route
-	http.HandleFunc("/", homePage)
+	// http.HandleFunc("/", homePage)
 	// articles route
-	http.HandleFunc("/articles", getAllArticles)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	// http.HandleFunc("/articles", getAllArticles)
+
+	// now utilizing Gorilla/Mux Routing
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", getAllArticles)
+
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
 	Articles = []Article{
         {Title: "Hello", Desc: "Article Description", Content: "Article Content"},
         {Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
