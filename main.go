c01b2b85ea50a445e69a95b8d72a9147b0620191
Jerry McKee Jr <jmckeejr@safehomesecurityinc.com>
2024-10-01 12:17:28 -0400

    created a function to retrieve just one article by its Id

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 // Creating an Article struct
 type Article struct {
+	Id      string `json:"Id"`
 	Title 	string `json:"Title"`
 	Desc  	string `json:"Description"`
 	Content string `json:"Content"`
@@ -34,6 +35,7 @@ func handleRequest(){
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/all", getAllArticles)
+	myRouter.HandleFunc("/article/{id}", getArticleById)
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
@@ -41,8 +43,8 @@ func handleRequest(){
 func main() {
 	fmt.Println("Rest API v2.0 - Mux Routers")
 	Articles = []Article{
-        {Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        {Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+        {Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        {Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
     }
 	handleRequest()
 }
@@ -51,4 +53,17 @@ func main() {
 func getAllArticles(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Endpoint Hit: getAllArticles")
 	json.NewEncoder(w).Encode(Articles)
+}
+
+// creating as function to return just a single article
+func getArticleById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	fmt.Fprintf(w, "Key: " + key)
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
 }
